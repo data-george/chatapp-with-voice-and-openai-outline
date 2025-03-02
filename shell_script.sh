f0fa65dd46f81b1459d8330e26052c78b21e657e
data-george <georgefrancispinto@gmail.com>
2025-03-02 14:34:39 -0500

    comment out the requirement commands from the shell script so that only the build and run docker commands are left

--- shell_script.sh
+++ shell_script.sh
@@ -1,4 +1,4 @@
-mkdir /home/project/chatapp-with-voice-and-openai/certs/
-cp /usr/local/share/ca-certificates/rootCA.crt /home/project/chatapp-with-voice-and-openai/certs/
+#mkdir /home/project/chatapp-with-voice-and-openai/certs/
+#cp /usr/local/share/ca-certificates/rootCA.crt /home/project/chatapp-with-voice-and-openai/certs/
 docker build . -t voice-chatapp-powered-by-openai
 docker run -p 8000:8000 voice-chatapp-powered-by-openai
